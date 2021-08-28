55c1ccef5058523da7c672630d0c8c6b05c87d67
erick <ericklarsenn@gmail.com>
2021-08-28 16:44:43 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: ['./index.html', './src/**/*.tsx', './src/**/*.ts'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
