d24d04d349c6adb42962f75bf168053267319054
erick <ericklarsenn@gmail.com>
2021-08-28 16:53:23 +0700

    Set home screen

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const home: React.FC = () => {
-  return <div></div>;
+const Home: React.FC = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Hi Guys!</h1>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
